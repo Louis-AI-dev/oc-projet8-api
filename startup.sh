f25e328fcd0dc75e7a9e582e792d2d1cacebfe97
Louis-AI-dev <cotinat.louis@gmail.com>
2025-08-07 11:33:37 +0100

    Ajout de log

--- startup.sh
+++ startup.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+echo "------ Startup script is running ------"
+
+# Optionnel : affiche le contenu du répertoire courant pour vérifier que tout est là
+echo "Current directory contents:"
+ls -la
+
+# Lancement de Gunicorn avec FastAPI
 gunicorn --bind=0.0.0.0:8000 --timeout 600 api:app
+
+echo "------ Gunicorn has been launched ------"
